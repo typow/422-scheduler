9902f7e66e6d7ea6d8bc6f580581f39f47350095
tyguy24@uw.edu <tyguy24@uw.edu@d20f82e6-1609-2033-7ba2-46b23687846d>
2013-06-03 00:15:03 +0000

    Made process.c pcb.c files as well as mutex.c and screw mobus

--- trunk/Scheduler/422 scheduler/PCB.h
+++ trunk/Scheduler/422 scheduler/PCB.h
@@ -12,7 +12,7 @@ typedef struct {
 	int next_step;		// this is the step count that the CPU had gotten to when this process was
 						// prempted (like a PC register value)
 	int state;			// e.g. 0=running, 1=ready, 2=interrupted, 3=blocked
-	//ProcessStr proc;    // pointer to the actual process
+	ProcessPtr proc;    // pointer to the actual process
 	int waiting_on;		// which queue is it in if it is waiting on something (blocked)
 	int owns;			// which mutex lock does it own
 } PCBStr;
@@ -20,4 +20,4 @@ typedef struct {
 
 typedef PCBStr *PCBPtr;
 
-PCBPtr pcbConstructor(int);
+PCBPtr pcbConstructor(int, ProcessPtr);

df435ce1fe4d8a94fafb850158e3663e05c66ff4
ainsley.herndon@gmail.com <ainsley.herndon@gmail.com@d20f82e6-1609-2033-7ba2-46b23687846d>
2013-06-05 00:47:59 +0000

    Added setGlobalVariables()

--- Scheduler/422 scheduler/main.c
+++ Scheduler/422 scheduler/main.c
@@ -2,7 +2,7 @@
  * main.c
  *
  *  Created on: Jun 3, 2013
- *      Author: TyPow
+ *      Author: TyPow, Herndon
  */
 
 #include "Global.h"
@@ -16,8 +16,51 @@
 #include <windows.h>
 
 void *timerInterrupt();
+void setGlobalVariables();
 
-int main() {
+void setGlobalVariables(int argc, const char* argv[]) {
+	int i;
+	for(i = 1; i < argc; i+=2 )
+	{
+		if (i+1 < argc) { // check that num for argument exists
+			if (strcmp( argv[i], "-k") == 0) {
+				// keyboard input processes
+				numOfKeyboardProc = atoi(argv[i+1]);
+			} else if (strcmp( argv[i], "-p") == 0) {
+				// compute bound process
+				numOfProc = atoi(argv[i+1]);
+			} else if (strcmp( argv[i], "-io") == 0) {
+				// input/output bound process
+				numOfIOProc = atoi(argv[i+1]);
+			} else if (strcmp( argv[i], "-pc") == 0) {
+				// producer consumer pairs
+				numOfProducerConsumerProc = atoi(argv[i+1]);
+			}
+		}
+	}
+
+	// the values are correct
+	if (numOfKeyboardProc > 0 && numOfProc > 0 && numOfProducerConsumerProc > 0 &&
+			numOfKeyboardProc > 0 && numOfIOProc > 0) {
+		// CONTINUE
+	} else {
+		// INCORRECT VALUE. throw exception?
+	}
+}
+
+void *timerInterrupt(void) {
+	while(1 == 1) {
+		Sleep(3000);
+		timerInt = 1;
+	}
+	return 0;
+}
+
+int main(int argc, const char* argv[]) {
+
+	// Example for assignment: -p 22 -k 2 -io 3 -pc 3
+	setGlobalVariables(argc, argv);
+
 	pthread_t timer;
 	pthread_create(&timer, NULL, timerInterrupt, NULL);
 	//int i = 1;
@@ -27,13 +70,6 @@ int main() {
 			timerInt = 0;
 		}
 	}
-	return 0;
-}
 
-void *timerInterrupt(void) {
-	while(1 == 1) {
-		Sleep(3000);
-		timerInt = 1;
-	}
 	return 0;
 }

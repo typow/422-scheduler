6fc4232e765d370234720c556a9680da4fe3da2b
ainsley.herndon@gmail.com <ainsley.herndon@gmail.com@d20f82e6-1609-2033-7ba2-46b23687846d>
2013-06-06 01:32:37 +0000

    added printing to log

--- Scheduler/422 scheduler/main.c
+++ Scheduler/422 scheduler/main.c
@@ -15,6 +15,12 @@
 #include <pthread.h>
 #include <windows.h>
 
+
+FILE *fp;
+char outputFilename[] = "Log.txt";
+
+void setUpLogFile();
+void printToLog();
 void *timerInterrupt();
 void setGlobalVariables();
 
@@ -48,6 +54,35 @@ void setGlobalVariables(int argc, const char* argv[]) {
 	}
 }
 
+void setUpLogFile() {
+
+	// if file already exists, it will be replaced, otherwise a new file will be created
+	fp = fopen(outputFilename, "w");
+
+	if (fp == NULL) {
+		// this should not happen but if it does, we should probably
+		// indicate to the user
+	  fprintf(stderr, "Can't create output file %s!\n",
+	          outputFilename);
+	  //exit(1);  // don't think we want to do this
+	}
+
+	fclose(fp);
+}
+
+
+void printToLog(char lineToPrint[]) {
+	fp = fopen(outputFilename, "a"); // to append to the existing log file
+
+	if (fp != NULL) {
+		//size_t len = len = strlen(lineToPrint);
+		//fwrite(lineToPrint, len, 1, fp);
+		fputs(lineToPrint, fp);
+	}
+
+	fclose(fp);
+}
+
 void *timerInterrupt(void) {
 	while(1 == 1) {
 		Sleep(3000);
@@ -60,6 +95,7 @@ int main(int argc, const char* argv[]) {
 
 	// Example for assignment: -p 22 -k 2 -io 3 -pc 3
 	setGlobalVariables(argc, argv);
+	setUpLogFile();
 
 	pthread_t timer;
 	pthread_create(&timer, NULL, timerInterrupt, NULL);
@@ -70,6 +106,7 @@ int main(int argc, const char* argv[]) {
 			timerInt = 0;
 		}
 	}
+	printToLog();
 
 	return 0;
 }
